bae12770bc9322a9142316348315c411fa37bcc0
“ravikumar9555” <ravikumar7518989121@email.com>
2025-12-02 21:51:36 +0530

    Completed CityRepository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,9 +4,12 @@ const { City } = require('../models/index');
 class CityRepository {
     async createCity({ name }){
         try {
-            const city = await City.create({name});
+            const city = await City.create({
+                name
+            });
             return city;
         } catch (error) {
+            console.log("Something went wrong");
             throw {error};
         }
     }
@@ -18,9 +21,37 @@ class CityRepository {
                 id : cityId
             }
            });
+           return true;
         } catch (error) {
+             console.log("Something went wrong");
             throw{error};
         }
+    }
+
+    async updateCity(cityId , data){
+        try {
+            const city = City.update(data , {
+                where :{
+                    id : cityId
+                }
+            })
+            return city;
+        } catch (error) {
+             console.log("Something went wrong");
+             throw(error);
+        }
+          
+    }
+
+    async getCity(cityId){
+          try {
+            const city = await City.findByPk(cityId)
+            return city;
+          } catch (error) {
+             console.log("Something went wrong");
+             throw(error);
+            
+          }
     }
 }
 module.exports = CityRepository;
