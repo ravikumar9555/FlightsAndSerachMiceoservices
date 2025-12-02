d6766c12cfd405c0d0a90c880a18822688d71f60
“ravikumar9555” <ravikumar7518989121@email.com>
2025-12-02 13:33:43 +0530

    Server Confiq

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const {PORT} = require('./confiq/serverConfiq')
+const bodyparser = require('body-parser')
 
 const setupAndStartServer = async () => {
     
     const app = express();
 
+    app.use(bodyparser.json())
+    app.use(bodyparser.urlencoded({extended: true }))
+
     app.listen(PORT , () => {
         console.log(`Server started at ${PORT}`);
-    
+     
     })
      
 } 
-setupAndStartServer();
+setupAndStartServer(); 
